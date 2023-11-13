75a304f0000453a6a105b65bee621158b230332a
yoones <yoonesakbari3@gmail.com>
2023-11-13 09:43:12 -0800

    end 1 window

--- Hesab.py
+++ Hesab.py
@@ -1,1 +1,15 @@
-from PyQt6.Qtwidgets import QApplication
+from PyQt6.Qtwidgets import QApplication , Qwidget, Qpushbutton
+
+app = QApplication([])
+
+window = Qwidget
+window.setgeomtry(0,0,100,100)
+
+for i in range(10):
+
+      btn = Qpushbutton("click",parent = window)
+      btn.move(i*10,100)
+
+window.show()
+
+app.exe()
